96b5711cd0512b7041b0c6159c88c585ed9e3027
John Doe <johndoe@example.com>
2017-11-13 09:38:02 -0700

    second commit

--- magic_8ball.rb
+++ magic_8ball.rb
@@ -1,52 +1,74 @@
 def welcome
   puts "Hello welcome to the magic 8 ball"
+  puts "Enter quit when you want to exit"
 end
 
 $answers = [
   "Thats not acceptable",
   "Ask again later",
-  "My reply is No"
+  "My reply is No",
+  "You should ba ashamed!",
+  "You call that a question",
+  "You should",
+  "Thats awesome"
 ]
+
+ARGV 
+
+
+def check_arg
+  command = ARGV.first
+  ARGV.clear
+  if command == "add_answers"
+    
+  add_answers
+  end
+
+
+end 
+
 
 def random_answer
   $answers.sample(1)
 end
 
+def add_answers
+  puts "Please add your own answer!"
+  
+  $answers.push gets.strip
+# add to the arry $answers
+end
+
+def reset_answers
+
+  #reset the arry answers to original
+end
+
+def print_answers
+  puts $answers
+  #print the answers to screen
+end 
+
+
 def app
+  check_arg
   puts "Enter your question?"
-  if gets.downcase.strip == "quit"
+  question = gets.downcase.strip
+  if question == "add_answers"
+    add_answers
+  elsif question == "print_answers"
+    print_answers
+  elsif  question == "quit"
+    puts "Good bye!!!! Come Again!!!!"
     exit
-  end
+  else 
   puts random_answer
-  #tell user to enter ? or to quit
-  # user inputs a question
-  #program rndomly pics answer
-  #print out answer
-  #return to to user input or call
-  # enter quit to exit
+  
+
+  end
+  
   app
 end
 
 welcome
 app
-
-=begin
-Basic Objectives:
-
-- User inputs question
-- Computer outputs random answer
-- User inputs "QUIT"
-- Computer outputs a goodbye message and exits
-Bonus Objectives:
-
-- ability to add more answers:
-- via easter egg question ("add_answers")
-- do not let them add the same answer if the eight ball already has that answer
-- ability to reset answers back to the original bank (hint: think arr.clone)
-- via easter egg question ("reset_answers")
-- ability to have eight ball print all answers
-- via easter egg question ("print_answers")
--ability to use script arguments when the magic eight ball is started to do bonus functionality above
-- for example ruby magic_eight.RB add_answers
-this would start your script but instead of running the regular way you would be prompted to add answers first
-=end
